e39bced3a385473eb9c53762e24e17c47d5e76bf
jennaliu <jyliu0001@gmail.com>
2022-10-30 15:02:07 -0400

    Image class

---
+++ src/Brighten.java
@@ -0,0 +1,19 @@
+/**
+ * This class is used to brighten images.
+ */
+public class Brighten implements ImageFunctionObject {
+
+  private final int amount;
+
+  /**
+   * The constructor which sets the given amount.
+   * @param amount How much to brighten the image by
+   */
+  public Brighten(int amount) {
+    this.amount = amount;
+  }
+  @Override
+  public void apply(Image image) {
+    // code that brightens the image
+  }
+}
