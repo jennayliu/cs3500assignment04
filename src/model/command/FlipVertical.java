2dc257f6502d0493bd0f11bdffbfdb24114c5299
jennaliu <jyliu0001@gmail.com>
2022-11-02 14:06:21 -0400

    added tests, fixed loops

--- src/model/command/FlipVertical.java
+++ src/model/command/FlipVertical.java
@@ -16,7 +16,10 @@ public class FlipVertical implements ImageFunctionObject {
 
     for (int r = 0; r < image.length / 2; r++) {
       for (int c = 0; c < image[0].length; c ++) {
+        PixelRGB copy = new PixelRGB(image[r][c].getRed(), image[r][c].getGreen(),
+                image[r][c].getBlue(), image[r][c].getMax());
         image[r][c] = image[image[0].length - 1 - r][c];
+        image[image[0].length - 1 - r][c] = copy;
       }
     }
 
