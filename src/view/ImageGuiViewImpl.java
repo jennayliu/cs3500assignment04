5e67e28a31f7cc202ab472003e8693fc9c359f87
Qinrong Cui <qinrongcui@gmail.com>
2022-11-20 17:20:28 -0500

    Add: gui view for commands

--- src/view/ImageGuiViewImpl.java
+++ src/view/ImageGuiViewImpl.java
@@ -1,15 +1,17 @@
 package view;
 
 import java.awt.*;
+import java.io.File;
 
 import javax.swing.*;
 
-public class ImageGuiViewImpl implements ImageGuiView{
+public class ImageGuiViewImpl implements ImageGuiView {
 
   private final int width;
   private final int height;
 
   private final JFrame baseFrame;
+
 
 
   public ImageGuiViewImpl() {
@@ -21,7 +23,7 @@ public class ImageGuiViewImpl implements ImageGuiView{
 
   @Override
   public void initialize() {
-    this.baseFrame.setSize(1920,1080);
+    this.baseFrame.setSize(1920, 1080);
 
     JPanel mainPanel = new JPanel();
     this.initializePanel(mainPanel);
@@ -31,8 +33,8 @@ public class ImageGuiViewImpl implements ImageGuiView{
     this.baseFrame.setVisible(true);
   }
 
-  private void initializePanel(JPanel mainPanel){
-    mainPanel.setSize(1920,1080);
+  private void initializePanel(JPanel mainPanel) {
+    mainPanel.setSize(1920, 1080);
 
     mainPanel.setLayout(new BorderLayout());
     JPanel leftPanel = new JPanel();
@@ -41,7 +43,7 @@ public class ImageGuiViewImpl implements ImageGuiView{
     JPanel rightPanel = new JPanel();
 
 
-    this.initialCommandPanel(leftPanel);
+    this.initialLeftPanel(leftPanel);
 
     JPanel centerPanel = new JPanel();
 
@@ -50,26 +52,101 @@ public class ImageGuiViewImpl implements ImageGuiView{
     mainPanel.add(centerPanel);
   }
 
-  private void initialCommandPanel(JPanel leftPanel){
+  /**
+   * This method creates the command Panel(on the left), which is use to modify the picture.
+   *
+   * @param leftPanel the left panel for placing these panels
+   */
+  private void initialLeftPanel(JPanel leftPanel) {
     //for elements to be arranged vertically within this panel
     leftPanel.setLayout(new BoxLayout(leftPanel, BoxLayout.PAGE_AXIS));
 //    //scroll bars around this main panel
 //    JScrollPane rightScrollPane = new JScrollPane(rightPanel);
 //    rightPanel.add(rightScrollPane);
 
+    // This part is for Load & Save
+    JPanel loadSavePanel = new JPanel();
+    loadSavePanel.setBorder(BorderFactory.createTitledBorder("Load & Save"));
+    loadSavePanel.setLayout(new GridLayout(1,2));
+    leftPanel.add(loadSavePanel);
+
+    JButton loadButton = new JButton("Load");
+    loadButton.addActionListener(e -> {
+      JFileChooser fc = new JFileChooser();
+      fc.setDialogTitle("Choose image to modify");
+      fc.showOpenDialog(null); // what this means??
+      //get the selected file
+      File image = fc.getSelectedFile();
+
+      if(image != null){
+        String imagePath = image.getAbsolutePath();
+        String imageName = image.getName();
+      } else {
+        throw new IllegalArgumentException("Invalid image");
+      }
+
+    });
+    JButton saveButton = new JButton("Save");
+    loadSavePanel.add(loadButton);
+    loadSavePanel.add(saveButton);
+
+    // This part is for brighten & darken
+    JPanel brightenPanel = new JPanel();
+    brightenPanel.setBorder(BorderFactory.createTitledBorder("Brighten & Darken"));
+    brightenPanel.setLayout(new GridLayout(2,1));
+    leftPanel.add(brightenPanel);
+
+    JSlider brightenSlider = new JSlider(-255, 255,0);
+    brightenSlider.setPaintTicks(true);
+    brightenSlider.setMajorTickSpacing(51);
+    brightenSlider.setMinorTickSpacing(10);
+    brightenSlider.setPaintLabels(true);
+    brightenPanel.add(brightenSlider);
+
+    JButton brightenButton = new JButton("Brighten");
+    brightenPanel.add(brightenButton);
+
+    // This part is for GreyScale
     JPanel greyScalePanel = new JPanel();
     greyScalePanel.setBorder(BorderFactory.createTitledBorder("GreyScale"));
-    greyScalePanel.setLayout(new BoxLayout(greyScalePanel, BoxLayout.PAGE_AXIS));
+    greyScalePanel.setLayout(new GridLayout(2, 1));
     leftPanel.add(greyScalePanel);
 
-    String[] options = {"Like it", "Love it", "Gotta have it"};
+    String[] options = {"N/A", "Red Component", "Green Component", "Blue Component",
+            "Value Component", "Intensity Component", "Luma Component", "Greyscale"};
     JComboBox<String> combobox = new JComboBox<String>();
-    //the event listener when an option is selected
-    combobox.setActionCommand("Size options");
+    combobox.setActionCommand("Greyscale options");
     for (int i = 0; i < options.length; i++) {
       combobox.addItem(options[i]);
     }
     greyScalePanel.add(combobox);
+
+    JButton greyScaleButton = new JButton("Greyscale");
+    //OKButton.addActionListener(new MyAction());
+    greyScalePanel.add(greyScaleButton);
+
+    // This part is for flip
+    JPanel flipPanel = new JPanel();
+    flipPanel.setBorder(BorderFactory.createTitledBorder("Flip"));
+    flipPanel.setLayout(new GridLayout(2, 1));
+    leftPanel.add(flipPanel);
+
+    JButton verFlipButton = new JButton("Vertical Flip");
+    JButton hoFlipButton = new JButton("Horizontal Flip");
+    //OKButton.addActionListener(new MyAction());
+    flipPanel.add(verFlipButton);
+    flipPanel.add(hoFlipButton);
+
+    // This part is for flip
+    JPanel sepiaPenal = new JPanel();
+    sepiaPenal.setBorder(BorderFactory.createTitledBorder("Sepia"));
+    sepiaPenal.setLayout(new GridLayout(1, 1));
+    leftPanel.add(sepiaPenal);
+
+    JButton sepiaButton = new JButton("Sepia");
+    //OKButton.addActionListener(new MyAction());
+    sepiaPenal.add(sepiaButton);
+
 
   }
 
